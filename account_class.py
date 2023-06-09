9380c6d500f6415847fe6086c353e983353ba04b
josemsantiago <69047359+josemsantiago@users.noreply.github.com>
2023-06-08 20:34:00 -0700

    Addition of stock reporting functionality

--- account_class.py
+++ account_class.py
@@ -3,21 +3,28 @@
 # -*- coding: utf-8 -*-
 from stock_class import Stock
 
-class Retirement_Account :
-    def __init__(self, balance,number) :
+
+class Retirement_Account:
+    def __init__(self, balance, number):
         self.balance = balance
         self.number = number
-class Traditional (Retirement_Account) : 
-    def __init__(self,balance,number):
-        Retirement_Account.__init__(self, balance,number)
+
+
+class Traditional (Retirement_Account):
+    def __init__(self, balance, number):
+        Retirement_Account.__init__(self, balance, number)
         self.Stock_List = []
-    def add_stock(self,stock_data) :
+
+    def add_stock(self, stock_data):
         self.Stock_List.append(stock_data)
-class Robo (Retirement_Account): 
-    def __init__(self,balance,number,years):
-        Retirement_Account.__init__(self,balance,number)
+
+
+class Robo (Retirement_Account):
+    def __init__(self, balance, number, years):
+        Retirement_Account.__init__(self, balance, number)
         self.years = years
-    def investment_return(self) :
+
+    def investment_return(self):
         return (self.years*self.balance*1.05)
 
 # Unit Test - Do Not Change Code Below This Line *** *** *** *** *** *** *** *** ***
@@ -25,34 +32,35 @@ class Robo (Retirement_Account):
 # Run this to test your class code. Once you have eliminated all errors, you are
 # ready to continue with the next part of the project.
 
+
 def main():
     error_count = 0
     error_list = []
     print("Unit Testing Starting---")
     # Test Add Traditional
-    stock_list =[]
+    stock_list = []
     testStock = Stock("TEST", "Testcompany", 200)
     stock_list.append(testStock)
-    print("Testing Add Retirement Account...",end="")
+    print("Testing Add Retirement Account...", end="")
     try:
-        testRetire = Retirement_Account(200,"12345")
+        testRetire = Retirement_Account(200, "12345")
         print("Successful!")
     except:
         print("***Adding retirement Account Failed!")
         error_count = error_count+1
         error_list.append("Retirement Constructor Error")
-    print("Testing Add Traditional Account...",end="")
+    print("Testing Add Traditional Account...", end="")
     try:
-        testTraditional = Traditional(200,"12345")
+        testTraditional = Traditional(200, "12345")
         testTraditional.add_stock(stock_list)
         print("Successful!")
     except:
         print("***Adding Traditional Account Failed!")
         error_count = error_count+1
         error_list.append("Traditional Constructor Error")
-   
+
     # Test Change Balance
-    print("Test Change Balance...",end="")
+    print("Test Change Balance...", end="")
     try:
         testTraditional.balance = 1000
         if testTraditional.balance == 1000:
@@ -65,9 +73,9 @@ def main():
         print("***ERROR! Balance change failed.")
         error_count = error_count+1
         error_list.append("Balance Change Failure")
-        
+
     # Test Change Number
-    print("Test Change Number...",end="")
+    print("Test Change Number...", end="")
     try:
         testTraditional.number = "99999"
         if testTraditional.number == "99999":
@@ -80,24 +88,21 @@ def main():
         print("***ERROR! Number change failed.")
         error_count = error_count+1
         error_list.append("Number Change Failure")
-        
-        
-        
-        
-    print("Testing Add Robo Account...",end="")
+
+    print("Testing Add Robo Account...", end="")
     try:
-        testRobo = Robo(200,"12345",5)
+        testRobo = Robo(200, "12345", 5)
         print("Successful!")
     except:
         print("***Adding Robo Account Failed!")
         error_count = error_count+1
         error_list.append("Robo Constructor Error")
-   
+
     # Test Change years
-    print("Test Change Balance...",end="")
+    print("Test Change Balance...", end="")
     try:
         testRobo.years = 1000
-    
+
         if testRobo.years == 1000:
             print("Successful!")
         else:
@@ -108,9 +113,9 @@ def main():
         print("***ERROR! Years change failed.")
         error_count = error_count+1
         error_list.append("Years Change Failure")
-        
+
     # Test investment return
-    print("Test investment return...",end="")
+    print("Test investment return...", end="")
     try:
         testRobo.years = 1000
         testRobo.balance = 1
@@ -123,8 +128,8 @@ def main():
     except:
         print("***ERROR! investment return failed.")
         error_count = error_count+1
-        error_list.append("Investment Return Failure") 
-   
+        error_list.append("Investment Return Failure")
+
     if (error_count) == 0:
         print("Congratulations - All Tests Passed")
     else:
@@ -133,8 +138,8 @@ def main():
             print(em)
     print("Goodbye")
 
+
 # Program Starts Here
 if __name__ == "__main__":
     # run unit testing only if run as a stand-alone script
     main()
-
--- stock_menu_v3.py
+++ stock_menu_v3.py
@@ -6,7 +6,7 @@ Created on Mon Aug 30 22:57:11 2021
 """
 from datetime import datetime
 from stock_class import Stock, DailyData
-from account_class import  Traditional, Robo
+from account_class import Traditional, Robo
 import matplotlib.pyplot as plt
 import csv
 
@@ -66,9 +66,9 @@ def list_stocks(stock_list):
 
 def add_stock_data(stock_list):
     print("Add Daily Stock Data ----")
-    print("Stock List: [",end="")
+    print("Stock List: [", end="")
     for stock in stock_list:
-        print(stock.symbol," ",end="")
+        print(stock.symbol, " ", end="")
     print("]")
     symbol = input("Which stock do you want to use?: ").upper()
     found = False
@@ -77,7 +77,7 @@ def add_stock_data(stock_list):
             found = True
             current_stock = stock
     if found == True:
-        print("Ready to add data for: ",symbol)
+        print("Ready to add data for: ", symbol)
         print("Enter Data Separated by Commas - Do Not use Spaces")
         print("Enter a Blank Line to Quit")
         print("Enter Date,Price,Volume")
@@ -85,8 +85,8 @@ def add_stock_data(stock_list):
         data = input("Enter Date,Price,Volume: ")
         while data != "":
             date, price, volume = data.split(",")
-            daily_data = DailyData(date,float(price),float(volume))
-          
+            daily_data = DailyData(date, float(price), float(volume))
+
             current_stock.add_data(daily_data)
             data = input("Enter Date,Price,Volume: ")
         print("Date Entry Complete")
@@ -95,39 +95,39 @@ def add_stock_data(stock_list):
     _ = input("Press Enter to Continue ***")
 
 
-
 def investment_type(stock_list):
     print("Investment Account ---")
     balance = float(input("What is your initial balance: "))
     number = input("What is your account number: ")
-    acct= input("Do you want a Traditional (t) or Robo (r) account: ")
+    acct = input("Do you want a Traditional (t) or Robo (r) account: ")
     if acct.lower() == "r":
         years = float(input("How many years until retirement: "))
         robo_acct = Robo(balance, number, years)
-        print("Your investment return is ",robo_acct.investment_return())
+        print("Your investment return is ", robo_acct.investment_return())
         print("\n\n")
     elif acct.lower() == "t":
         trad_acct = Traditional(balance, number)
-        temp_list=[]
+        temp_list = []
         print("Choose stocks from the list below: ")
         while True:
-            print("Stock List: [",end="")
+            print("Stock List: [", end="")
             for stock in stock_list:
-                print(stock.symbol," ",end="")
+                print(stock.symbol, " ", end="")
             print("]")
-            symbol = input("Which stock do you want to purchase, 0 to quit: ").upper()
-            if symbol =="0":
+            symbol = input(
+                "Which stock do you want to purchase, 0 to quit: ").upper()
+            if symbol == "0":
                 break
             shares = float(input("How many shares do you want to buy?: "))
             found = False
             for stock in stock_list:
-              if stock.symbol == symbol:
-                  found = True
-                  current_stock = stock
+                if stock.symbol == symbol:
+                    found = True
+                    current_stock = stock
             if found == True:
-                current_stock.shares += shares 
+                current_stock.shares += shares
                 temp_list.append(current_stock)
-                print("Bought ",shares,"of",symbol)
+                print("Bought ", shares, "of", symbol)
             else:
                 print("Symbol Not Found ***")
         trad_acct.add_stock(temp_list)
@@ -137,8 +137,8 @@ def investment_type(stock_list):
 
 
 def display_stock_chart(stock_list, symbol):
-    date =[]
-    price=[]
+    date = []
+    price = []
     volume = []
     company = ""
     for stock in stock_list:
@@ -148,11 +148,12 @@ def display_stock_chart(stock_list, symbol):
                 date.append(dailyData.date)
                 price.append(dailyData.close)
                 volume.append(dailyData.volume)
-    plt.plot(date,price)
+    plt.plot(date, price)
     plt.xlabel("Date")
     plt.ylabel("Price")
     plt.title(company)
     plt.show()
+
 
 def display_chart(stock_list):
     print("Stock Chart--")
@@ -168,21 +169,72 @@ def display_chart(stock_list):
             found = True
             current_stock = stock
     if found:
-        display_stock_chart(stock_list,current_stock.symbol)
-    else: 
+        display_stock_chart(stock_list, current_stock.symbol)
+    else:
         print("Symbol not found")
-        _= input("Press enter to continue")        
+        _ = input("Press enter to continue")
+
 
 def import_stock_csv(stock_list):
-    print("This method is under construction")
-
-    # Display Report
+    print("\nAdd historical data to a stock in the stock list\nStock list : [", end = "" )
+    for stock in stock_list:
+        print(stock.symbol, " ", end="")
+    print("]")
+    symbol = input("Enter stock symbol: ").upper()
+    filename = input("Enter the file name: ")
+    for stock in stock_list:
+        if stock.symbol == symbol:
+            with open(filename,newline='') as stockdata:
+                datareader = csv.reader(stockdata)
+                next(datareader)
+                for row in datareader:
+                    daily_data = DailyData(str(row[0]),float(row[4]),float(row[6]))
+                    stock.add_data(daily_data)
+    display_report(stock_list)
 
 
 def display_report(stock_list):
-    print("This method is under construction")
+    print("Stock report ----")
+    for stock in stock_list: 
+        print(f"Report for: {stock.symbol}, {stock.name} ")
+        print(f"Share: {stock.shares}")
+        count =0
+        price_total = 0
+        volume_total = 0
+        low_price = 9999999.99
+        high_price = 0
+        low_volume = 9999999999
+        high_volume = 0
+        for daily_data in stock.DataList: 
+            count +=1
+            price_total += daily_data.close
+            volume_total += daily_data.volume
+            if daily_data.close < low_price: 
+                low_price = daily_data.close
+            if daily_data.close > high_price:
+                high_price = daily_data.close
+            if daily_data.volume < low_volume:
+                low_volume = daily_data.volume
+            if daily_data.volume > high_volume:
+                high_volume = daily_data.volume
+            price_change = high_price - low_price
+            print(daily_data.date,daily_data.close,daily_data.volume)
 
-
+        if count > 0: 
+            print(f"Summary ----")
+            print(f"Low Price: ${low_price:,.2f}")
+            print(f"High Price: ${high_price:,.2f}")
+            print(f"Average Price: ${(price_total/count):,.2f}")
+            print(f"Low Volume: {low_volume}")
+            print(f"High Volumen Price: {high_volume}")
+            print(f"Change in price: {price_change:,.2f}")
+            print(f"Profit/Loss: ${(price_change*stock.shares):,.2f}")
+        else: 
+            print("****No daily history")
+        print("\n\n\n")
+    print("---Report Complete---")
+    _=input=("Press Enter to continue")
+        
 def main_menu(stock_list):
     option = ""
     while True:
